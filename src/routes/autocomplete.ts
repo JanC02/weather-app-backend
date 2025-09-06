a3319ffbf472b57b7faf2bb130dc80eff01d6d50
Jan Chrobak <jan.chrobak02@outlook.com>
2025-09-06 21:41:12 +0200

    feat: add caching for autocomplete

--- src/routes/autocomplete.ts
+++ src/routes/autocomplete.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import { config } from '../config.js';
 import { AutocompleteSchema } from '../types.js';
+import NodeCache from 'node-cache';
+import { removeDiacritics } from '../helpers/removeDiacritics.js';
 
 const RESULTS_MAX_COUNT = config.autocomplete.maxResults;
 const RESPONSE_LANG = config.autocomplete.responseLanguage;
 const AUTOCOMPLETE_URL = config.autocomplete.geocodingUrl;
+
+// 24h
+const cache = new NodeCache({ stdTTL: 86400 });
 
 if (!AUTOCOMPLETE_URL) {
     console.error('FATAL ERROR: AUTOCOMPLETE_URL is not defined.');
@@ -22,11 +27,21 @@ router.get('/autocomplete', async (req, res) => {
 
     const parsedQuery = result.data.q;
 
+    const normalizedQuery = removeDiacritics(parsedQuery);
+    const cacheKey = `autocomplete-${normalizedQuery}`;
+
     try {
+        const cachedData = cache.get(cacheKey);
+        if (cachedData) {
+            return res.status(200).json(cachedData);
+        }
+
         const response = await fetch(`${AUTOCOMPLETE_URL}?name=${parsedQuery}&count=${RESULTS_MAX_COUNT}&language=${RESPONSE_LANG}&format=json`);
 
         if(response.ok) {
             const data = await response.json();
+            cache.set(cacheKey, data);
+
             res.status(200).json(data);
         } else {
             res.status(response.status).json({ message: response.statusText });
